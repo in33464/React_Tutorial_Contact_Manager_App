28c4137115cd05f4a859e33bd39d0e9cd08cd1f9
Ramkumar N <ram.kumar@indiumsoft.com>
2022-10-28 15:51:06 +0530

    crud api integration

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { v4 as uuid } from "uuid";
+import api from "../api/contacts";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -7,16 +8,28 @@ import ContactList from "./ContactList";
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState(
-    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
-  );
+  const [contacts, setContacts] = useState([]);
 
-  const addContactHandler = (contact) => {
+  const retriveContacts = async () => {
+    const response = await api.get("/contacts");
+    return response.data;
+  };
+  const addContactHandler = async (contact) => {
     console.log(contact);
-    setContacts([...contacts, { id: uuid(), ...contact }]);
+    const request = {
+      id: uuid(),
+      ...contact,
+    };
+
+    
+
+    const response = await api.post("/contacts", request)
+    console.log(response);
+    setContacts([...contacts, response.data]);
   };
 
-  const removeContactHandler = (id) => {
+  const removeContactHandler = async (id) => {
+    await api.delete(`/contacts/${id}`);
     const newContactList = contacts.filter((contact) => {
       return contact.id !== id;
     });
@@ -24,13 +37,19 @@ function App() {
     setContacts(newContactList);
   };
 
-  // useEffect(() => {
+   useEffect(() => {
   //   const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
   //   if (retriveContacts) setContacts(retriveContacts);
-  // }, []);
+  const getAllCOntacts = async() => {
+    const allContacts = await retriveContacts();
+    if(allContacts) setContacts(allContacts);
+  };
+  getAllCOntacts();
+}, []);
+
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+    //localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
   }, [contacts]);
 
   return (
